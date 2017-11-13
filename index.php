60b6474e4b57e7cb6dca003c99767e8a330c41f1
Eduardo f.O Rdrigues <dudacriativo@hotmail.com>
2017-11-13 00:49:05 -0200

    ok

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ echo "Seja bem vindo";
 echo "Meu primeiro contato com PHP";
 echo "PHP";
 
-
+<<<<<<< HEAD
+=======
 echo "olá teste";
 
 echo "add outro teste";
 
-
+>>>>>>> 93174beb51c2f224a3f8afc15e0d7ca36cb11099
 
 ?>
