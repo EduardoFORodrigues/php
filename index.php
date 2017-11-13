93174beb51c2f224a3f8afc15e0d7ca36cb11099
EduardoFORodrigues <33601272+EduardoFORodrigues@users.noreply.github.com>
2017-11-13 00:47:40 -0200

    outro teste

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ echo "Seja bem vindo";
 
 echo "Meu primeiro contato com PHP";
 echo "PHP";
-<<<<<<< HEAD
-=======
+
 echo "olá teste";
-=======
->>>>>>> d7f45075c1576047a2f38bc35d3552585250c43f
+
+echo "add outro teste";
+
 
 ?>
