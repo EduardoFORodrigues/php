a5e29fa30a5932b0ca8b1fe024be2ac7347133ff
Eduardo f.O Rdrigues <dudacriativo@hotmail.com>
2017-11-13 00:42:59 -0200

    outra alteracao

--- index.php
+++ index.php
@@ -5,10 +5,6 @@ echo "Seja bem vindo";
 
 echo "Meu primeiro contato com PHP";
 echo "PHP";
-<<<<<<< HEAD
-=======
-echo "olá teste";
-=======
->>>>>>> d7f45075c1576047a2f38bc35d3552585250c43f
+
 
 ?>
