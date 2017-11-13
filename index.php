d7f45075c1576047a2f38bc35d3552585250c43f
EduardoFORodrigues <33601272+EduardoFORodrigues@users.noreply.github.com>
2017-11-13 00:40:33 -0200

    outra alteração

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 echo "Hello Word!";
 echo "Seja bem vindo";
-<<<<<<< HEAD
+
 
 echo "Meu primeiro contato com PHP";
 echo "PHP";
-=======
-echo "olá teste";
-echo "Boa noite vamos Programa";
->>>>>>> 728ff41967aa2528c23dfccea2f77a82784ed237
+
 ?>
